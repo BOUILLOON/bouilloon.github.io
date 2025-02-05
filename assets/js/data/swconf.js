e3229e9b2ea73471a7d8f2b57c6d2059b7d5bb48
BOUILLOON <r.nantenaina@outlook.fr>
2025-02-05 07:51:40 +0000

    Update

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738741730',resources: [
+    cacheName: 'chirpy-1738741900',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
