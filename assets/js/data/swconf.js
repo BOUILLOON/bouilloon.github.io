4917a0957793b3ebd83bcb48c7a65c4530351a77
BOUILLOON <r.nantenaina@outlook.fr>
2025-02-05 08:08:10 +0000

    updating static sources

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738742684',resources: [
+    cacheName: 'chirpy-1738742890',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
